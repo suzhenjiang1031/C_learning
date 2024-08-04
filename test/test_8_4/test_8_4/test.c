99201df992139c532a8251121c5637ece2fa407b
suzhenjiang <suzhenjiang1031@gmail.com>
2024-08-04 21:57:43 +0800

    qsort

---
+++ test/test_8_4/test_8_4/test.c
@@ -0,0 +1,102 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int int_cmp(const void* p1, const void* p2)
+//{
+//	return (*(int*)p1 - *(int*)p2);
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+//	int i = 0;
+//	qsort(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), int_cmp);
+//	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//};
+//
+//int cmp_stu_by_age(const void* e1, const* e2)
+//{
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+//
+//int cmp_stu_by_name(const void* e1, const void* e2)
+//{
+//	return strcmp(((struct Stu*)e1)->name, ((struct Stu*)e2)->name);
+//}
+//
+//void test2()
+//{
+//	struct Stu s[] = { {"zhangshan", 20},{"lisu", 30},{"wangwu",15} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), cmp_stu_by_age);
+//}
+//
+//void test3()
+//{
+//	struct Stu s[] = { {"zhangshan", 20},{"lisi", 30}, {"wangwu",15} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), cmp_stu_by_name);
+//}
+//
+//int main()
+//{
+//	test2();
+//	test3();
+//	return 0;
+//}
+
+int cmp(const void* p1, const void* p2)
+{
+	return (*(int*)p1) - (*(int*)p2);
+}
+
+void _swap(void* p1, void* p2, int size)
+{
+	int i = 0;
+	for (i = 0; i < size; i++)
+	{
+		char tmp = *((char*)p1 + i);
+		*((char*)p1 + i) = *((char*)p2 + i);
+		*((char*)p2 + i) = tmp;
+	}
+}
+
+void bubble(void* base, int count, int size, int(*cmp)(void*, void*))
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < count - 1; i++)
+	{
+		for (j = 0; j < count - i - 1; j++)
+		{
+			if (cmp((char*)base + j * size, (char*)base + (j + 1) * size) > 0)
+			{ 
+				_swap((char*)base + j * size, (char*)base + (j + 1) * size, size);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+	int i = 0;
+	bubble(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), cmp);
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
