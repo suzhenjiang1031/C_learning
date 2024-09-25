92059002f8d3b7ce9dee64a71a79a7f1ed786e88
suzhenjiang <suzhenjiang1031@gmail.com>
2024-09-25 21:27:25 +0800

    test

---
+++ test7_12/game/game/game.c
@@ -0,0 +1,38 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include "game.h"
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols, char set)
+{
+	int i = 0;
+	for (i = 0; i < rows; i++)
+	{
+		int j = 0;
+		for (j = 0; j < cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+}
+
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	printf("--------É¨À×ÓÎÏ·--------");
+	for (i = 0; i <= col; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	for (i = 1; i <= row; i++)
+	{
+		printf("%d ", i);
+		int j = 0;
+		for (j = 1; j <= col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+
+
+}
